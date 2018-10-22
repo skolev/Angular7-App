7374928b10bb2c321135c84cd108468c5148180c
Svetoslav Kolev <svetoslav.kolev@accedia.com>
2018-10-22 15:51:20 +0300

    Added content to recipes components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://gdsit.cdn-immedia.net/2017/01/CARNE.jpg'),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://gdsit.cdn-immedia.net/2017/01/CARNE.jpg')
+  ];
 
   constructor() { }
 
